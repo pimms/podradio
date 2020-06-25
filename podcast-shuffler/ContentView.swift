5d02f8d0adf972cc83bfda78da2bed98679764eb
Joakim Stien <joakim.stien@gmail.com>
2020-06-25 13:58:36 +0200

    Initial Commit

---
+++ podcast-shuffler/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  podcast-shuffler
+//
+//  Created by pimms on 25/06/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ podcast-shuffler/podcast_shufflerApp.swift
@@ -0,0 +1,17 @@
+//
+//  podcast_shufflerApp.swift
+//  podcast-shuffler
+//
+//  Created by pimms on 25/06/2020.
+//
+
+import SwiftUI
+
+@main
+struct podcast_shufflerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
