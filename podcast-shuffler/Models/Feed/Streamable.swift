d55d629ab1a79beb992e01f80e83c602637bce08
Joakim Stien <joakim.stien@icloud.com>
2020-12-29 10:52:47 +0100

    Restructuring

--- podcast-shuffler/Models/Feed/Streamable.swift
+++ podcast-shuffler/Models/Feed/Streamable.swift
@@ -1,8 +1,5 @@
 import Foundation
 
-/// TODO
-/// StreamPoints should be configured with a time offset.
-/// The name is also quite rubbish.
 protocol Streamable {
     var episode: Episode { get }
     var nextStreamable: Streamable { get }
